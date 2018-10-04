30e135e17a3049c955664a6c67f6f51ec6447187
matt2tech <mlipsey@basecampcodingacademy.org>
2018-10-04 11:25:34 -0500

    created functions for each of the dropdown menus which show them

---
+++ dropdown.js
@@ -0,0 +1,7 @@
+function myFunction () {
+    document.querySelector('.dropdown-menu').classList.toggle('show');
+}
+
+function myFunction2 () {
+    document.querySelector('.drop-menu').classList.toggle('show')
+}
