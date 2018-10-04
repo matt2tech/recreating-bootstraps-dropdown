9d81c0480b33cd2ec46896a09e57200fb7842028
matt2tech <mlipsey@basecampcodingacademy.org>
2018-10-04 12:37:44 -0500

    Changed myFunction to work with any dropdown

--- dropdown.js
+++ dropdown.js
@@ -1,7 +1,3 @@
-function myFunction () {
-    document.querySelector('.dropdown-menu').classList.toggle('show');
-}
-
-function myFunction2 () {
-    document.querySelector('.drop-menu').classList.toggle('show')
+function myFunction (x) {
+    document.querySelector(x).classList.toggle('show');
 }
