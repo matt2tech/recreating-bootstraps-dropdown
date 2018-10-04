5738cc738c6580ceb3f01ead8e5c24b446fa3673
matt2tech <mlipsey@basecampcodingacademy.org>
2018-10-04 14:56:03 -0500

    success with dropdown box using javascript

--- dropdown.js
+++ dropdown.js
@@ -1,3 +1,11 @@
-function myFunction () {
-    var x = document.querySelectorAll('.dropdown');
+function myFunction() {
+    var x = document.querySelectorAll(".dropdown");
+    x.forEach(function(dropdown) {
+        var btn = dropdown.querySelector(".btn");
+        btn.addEventListener("click", function() {
+            dropdown.querySelector(".dropdown-menu").classList.toggle("show");
+        });
+    });
 }
+
+myFunction();
